703aef6e90da08d75e1fe2556c92b2decee23d31
unknown <hazem.ismail@hotmail.com>
2022-10-13 15:24:13 +0300

    1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Artisan::call('schedule:run');
         Paginator::useBootstrap();
         Model::preventLazyLoading();
     }
