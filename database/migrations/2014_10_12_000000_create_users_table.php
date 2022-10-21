46714e2e2b049da426c58cce1e872718acfd60e5
unknown <hazem.ismail@hotmail.com>
2022-10-21 12:25:38 +0300

    1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('active');
+            $table->foreignId('title_id')->nullable()->constrained('titles');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
