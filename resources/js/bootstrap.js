7dd8f4afbb8dab744e087f9973cc21dbe5f250f6
HazemIsmail-Prog <hazem.ismail@hotmail.com>
2023-02-25 23:42:30 +0300

    run dev

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -58,7 +58,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
+    key: 'local',
     wsHost: window.location.hostname,
     wsPort: 6001,
     forceTLS: false,
